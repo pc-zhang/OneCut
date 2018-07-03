f951072902e29b71ea7d4d0d77142d884b369d74
zhangpengcheng.zpc <zhangpengcheng.zpc@bytedance.com>
2018-07-03 13:20:01 +0800

    Initial Commit

---
+++ OneCut/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  OneCut
+//
+//  Created by zpc on 2018/7/3.
+//  Copyright © 2018年 zpc. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
